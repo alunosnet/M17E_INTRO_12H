3dea3e5158c8f40c6d9444ccac775c6c1e41d037
alunosNET <alunosnet@gmail.com>
2026-01-28 14:25:18 +0000

    Classes basedados e utilizadores

---
+++ M17E_INTRO_12H/Classes/Utilizadores.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace M17E_INTRO_12H.Classes
+{
+    public class Utilizadores
+    {
+        public int id;
+        public string email;
+        public string nome;
+        public string palavra_passe;
+        public int sal;
+        public int perfil;
+        BaseDados bd;
+        public Utilizadores()
+        {
+            bd = new BaseDados();
+        }
+
+        public bool VerificaLogin()
+        {
+            DataTable dados;
+            string sql = @"SELECT * FROM Utilizadores WHERE email=@email AND
+                        palavra_passe=HASHBYTES('SHA2_512',concat(@palavra_passe,sal))";
+            List<SqlParameter> parametros = new List<SqlParameter>()
+            {
+                new SqlParameter()
+                {
+                    ParameterName="@email",
+                    SqlDbType=SqlDbType.VarChar,
+                    Value=email
+                },
+                new SqlParameter()
+                {
+                    ParameterName="@palavra_passe",
+                    SqlDbType=SqlDbType.VarChar,
+                    Value=palavra_passe
+                }
+            };
+            dados = bd.devolveSQL(sql, parametros);
+            if (dados == null || dados.Rows.Count==0)
+                return false;
+            id = int.Parse(dados.Rows[0]["id"].ToString());
+            nome = dados.Rows[0]["nome"].ToString();
+            email = dados.Rows[0]["email"].ToString();
+            perfil = int.Parse(dados.Rows[0]["perfil"].ToString());
+            return true;
+        }
+
+    }
+}
--- M17E_INTRO_12H/Login.aspx.cs
+++ M17E_INTRO_12H/Login.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using M17E_INTRO_12H.Classes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,6 +13,21 @@ namespace M17E_INTRO_12H
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void bt_login_Click(object sender, EventArgs e)
+        {
+            //validar
+            if (tb_email.Text=="" || tb_password.Text=="")
+            {
+                lb_erro.Text = "Login falhou. Tente novamente.";
+                return;
+            }
+            //consulta à tabela de utilizadores
+            Utilizadores novo = new Utilizadores();
+            //TODO: CONTINUAR AQUI!!!!!!!!!!!
+            //Sessão - perfil, email, nome
+            //redirecionar o utilizador de acordo com perfil
         }
     }
 }
