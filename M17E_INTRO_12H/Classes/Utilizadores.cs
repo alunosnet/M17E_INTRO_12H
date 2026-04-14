57f428efd8c6225a943f88075e71be239024c363
alunosNET <alunosnet@gmail.com>
2026-04-14 10:04:16 +0100

    Testes unitários

--- M17E_INTRO_12H/Classes/Utilizadores.cs
+++ M17E_INTRO_12H/Classes/Utilizadores.cs
@@ -24,16 +24,15 @@ namespace M17E_INTRO_12H.Classes
         public bool VerificaLogin()
         {
             DataTable dados;
-            //Não FAZER
-            string sql = "SELECT * FROM Utilizadores WHERE email='"+email+"' AND palavra_passe=HASHBYTES('SHA2_512',concat(@palavra_passe,sal))";
+            string sql = "SELECT * FROM Utilizadores WHERE email=@email AND palavra_passe=HASHBYTES('SHA2_512',concat(@palavra_passe,sal))";
             List<SqlParameter> parametros = new List<SqlParameter>()
             {
-                //new SqlParameter()
-                //{
-                //    ParameterName="@email",
-                //    SqlDbType=SqlDbType.VarChar,
-                //    Value=email
-                //},
+                new SqlParameter()
+                {
+                    ParameterName="@email",
+                    SqlDbType=SqlDbType.VarChar,
+                    Value=email
+                },
                 new SqlParameter()
                 {
                     ParameterName="@palavra_passe",
